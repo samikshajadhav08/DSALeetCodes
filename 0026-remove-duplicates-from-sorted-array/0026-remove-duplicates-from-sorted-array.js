46721e29f9be248de1712a4d25cabc780d32f5a7
Samiksha Jadhav <developersamiksha1@gmail.com>
2026-09-10 10:37:11 +0530

    Time: 0 ms (100%), Space: 58.6 MB (15.2%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let k=1;
-    for(let j=1;j<nums.length;j++){
-        if(nums[j]!=nums[j-1]){
-            nums[k]=nums[j];
-            k++;
+    let i = 1;
+    for (let j = 1 ; j < nums.length ; j++){
+        if(nums[j] !== nums[i - 1] ){
+            nums[i] = nums[j];
+            i++;
         }
     }
-    return k;
+    return i;
 };
