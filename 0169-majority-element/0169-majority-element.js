e2eacea4b112e6504d9ad848487e66c7efe3753d
Samiksha Jadhav <developersamiksha1@gmail.com>
2026-09-11 19:02:19 +0530

    Time: 0 ms (100%), Space: 55.6 MB (89.35%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate=0;
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        if(count===0){
+            candidate=nums[i];
+        }
+        if(nums[i]===candidate){
+            count++;
+        }else{
+        count--;
+        }
+    }
+    return candidate;
+};
