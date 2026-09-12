97a3e0d337d4afc8eb29fba997f1a00e6ee58fd9
Samiksha Jadhav <developersamiksha1@gmail.com>
2026-09-12 16:29:09 +0530

    Time: 5 ms (63.83%), Space: 60.7 MB (24.33%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let left=0;
+    let right=s.length-1;
+
+    while(left<right){
+        while(left<right && !isAlphaNumeric(s[left])){
+            left++;
+        }
+        while(left<right && !isAlphaNumeric(s[right])){
+            right--;
+        }
+        if(s[left].toLowerCase() !== s[right].toLowerCase()){
+            return false;
+        }
+            left++;
+            right--;
+    }
+    return true;
+};
+function isAlphaNumeric(char){
+    return /^[a-zA-Z0-9]$/.test(char);
+};
