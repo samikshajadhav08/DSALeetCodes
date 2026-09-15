5d4e974ae246165c8dcaada1d633503f24eb589a
Samiksha Jadhav <developersamiksha1@gmail.com>
2026-09-15 15:26:23 +0530

    Time: 0 ms (100%), Space: 54.2 MB (52.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -6,16 +6,13 @@ var longestCommonPrefix = function(strs) {
     if(strs.length === 0){
         return "";
     }
-    let prefix=strs[0];
-    for(let i = 0; i < strs.length; i++){
-        let j = 0;
-        while(j < prefix.length && j < strs[i].length && prefix[j] === strs[i][j] ){
-            j++;
-        }
-        prefix=prefix.slice(0,j);
-        if(prefix.length===0){
-            return "";
-        }
+    for(let i = 0; i < strs[0].length; i++){
+        let curr = strs[0][i];
+        for(let j=1;j<strs.length;j++){
+            if(i>=strs[j].length || strs[j][i] != curr){
+                return strs[0].slice(0,i);
+            }
+        }   
     }
-    return prefix;
+    return strs[0];
 };
