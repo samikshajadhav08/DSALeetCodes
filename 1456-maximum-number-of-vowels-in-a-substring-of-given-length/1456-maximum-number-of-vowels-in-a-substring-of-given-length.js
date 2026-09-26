6acaa9f5c9a01bca3d3a1268dc52451d658bebb2
Samiksha Jadhav <developersamiksha1@gmail.com>
2026-09-26 09:41:01 +0530

    Time: 20 ms (42.79%), Space: 55.6 MB (79.66%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function(s, k) {
+  let vowels=new Set(["a","e","i","o","u"]);
+  let maxCount=0;
+  let count=0;
+  for(let i=0;i<k;i++){
+    if(vowels.has(s[i])){
+        count++;
+    }
+  }
+      maxCount=count;
+
+  for(let i=k;i<s.length;i++){
+    if(vowels.has(s[i])){
+        count++;
+    }
+    if(vowels.has(s[i-k])){
+        count--;
+    }
+    maxCount=Math.max(maxCount,count);
+  }
+  return maxCount;
+};
